f5244c9e8e722b243eb384283d67e4775d1a3d6b
John Inyang <john.i@azenia.com>
2021-04-11 08:47:56 +0100

    Added validation and Extensions

---
+++ GreenAfrica-API/Extensions/AppBuilderExtension.cs
@@ -0,0 +1,28 @@
+﻿using GreenAfrica.DataAccess;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GreenAfrica_API.Extensions
+{
+
+    public static class AppBuilderExtension
+    {
+        public static IApplicationBuilder SetupMigrations(this IApplicationBuilder app, IServiceProvider service)
+        {
+            var logger = service.GetService<ILogger<GreenAPIDbContext>>();
+            try
+            {
+                var context = service.GetService<GreenAPIDbContext>();
+                context.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+            }
+            return app;
+        }
+    }
+}
---
+++ GreenAfrica-API/Helper/ValidationHelper.cs
@@ -0,0 +1,176 @@
+﻿namespace GreenAfrica_API.Helper
+{
+
+    public static class ValidationHelper
+    {
+        public static (bool Valid, string PhoneNumber) ValidatePhoneNumber(string phoneNumber, string countryCode)
+        {
+            phoneNumber = phoneNumber.Replace("+", "");
+            if (string.IsNullOrEmpty(phoneNumber) || string.IsNullOrEmpty(countryCode))
+            {
+                return (false, phoneNumber);
+            }
+            if (phoneNumber != "000000")
+            {
+                switch (countryCode)
+                {
+                    case "KE":
+                        {
+                            if (!phoneNumber.StartsWith("254"))
+                            {
+
+                                if (phoneNumber.Length != 10 && phoneNumber.Length != 9)
+                                {
+                                    return (false, phoneNumber);
+                                }
+                                if (phoneNumber.StartsWith("0"))
+                                {
+                                    phoneNumber = phoneNumber.Substring(1, phoneNumber.Length - 1);
+                                }
+                                phoneNumber = "254" + phoneNumber;
+                            }
+                            return (true, phoneNumber);
+                        }
+                    case "UG":
+                        {
+                            if (!phoneNumber.StartsWith("256"))
+                            {
+                                if (phoneNumber.Length != 10 && phoneNumber.Length != 9)
+                                {
+                                    return (false, phoneNumber);
+                                }
+                                if (phoneNumber.StartsWith("0"))
+                                {
+                                    phoneNumber = phoneNumber.Substring(1, phoneNumber.Length - 1);
+                                }
+                                phoneNumber = "256" + phoneNumber;
+                            }
+                            return (true, phoneNumber);
+                        }
+                    case "CD":
+                        {
+                            if (!phoneNumber.StartsWith("243"))
+                            {
+                                if (phoneNumber.Length != 10 && phoneNumber.Length != 9)
+                                {
+                                    return (false, phoneNumber);
+                                }
+                                if (phoneNumber.StartsWith("0"))
+                                {
+                                    phoneNumber = phoneNumber.Substring(1, phoneNumber.Length - 1);
+                                }
+                                phoneNumber = "243" + phoneNumber;
+                            }
+                            return (true, phoneNumber);
+                        }
+                    case "TZ":
+                        {
+                            if (!phoneNumber.StartsWith("255"))
+                            {
+                                if (phoneNumber.Length != 10 && phoneNumber.Length != 9)
+                                {
+                                    return (false, phoneNumber);
+                                }
+                                if (phoneNumber.StartsWith("0"))
+                                {
+                                    phoneNumber = phoneNumber.Substring(1, phoneNumber.Length - 1);
+                                }
+                                phoneNumber = "255" + phoneNumber;
+                            }
+                            return (true, phoneNumber);
+                        }
+                    case "RW":
+                        {
+                            if (!phoneNumber.StartsWith("250"))
+                            {
+                                if (phoneNumber.Length != 10 && phoneNumber.Length != 9)
+                                {
+                                    return (false, phoneNumber);
+                                }
+                                if (phoneNumber.StartsWith("0"))
+                                {
+                                    phoneNumber = phoneNumber.Substring(1, phoneNumber.Length - 1);
+                                }
+                                phoneNumber = "250" + phoneNumber;
+                            }
+                            return (true, phoneNumber);
+                        }
+                    case "SS":
+                        {
+                            if (!phoneNumber.StartsWith("211"))
+                            {
+                                if (phoneNumber.Length != 10 && phoneNumber.Length != 9)
+                                {
+                                    return (false, phoneNumber);
+                                }
+                                if (phoneNumber.StartsWith("0"))
+                                {
+                                    phoneNumber = phoneNumber.Substring(1, phoneNumber.Length - 1);
+                                }
+                                phoneNumber = "211" + phoneNumber;
+                            }
+                            return (true, phoneNumber);
+                        }
+                    default:
+                        break;
+                }
+            }
+            return (true, phoneNumber);
+
+        }
+
+
+        public static string CleanPhoneNumber(string phoneNumber, string countryCode = "KE")
+        {
+            if (phoneNumber.StartsWith("0") && countryCode == "CD")
+            {
+                phoneNumber = "+243" + phoneNumber.Remove(0, 1);
+            }
+            if (countryCode == "KE")
+            {
+                // 254763679023
+                if (phoneNumber.StartsWith("000"))
+                {
+                    phoneNumber = "254" + phoneNumber.Remove(0, 3);
+                }
+                if (phoneNumber.StartsWith("00"))
+                {
+                    phoneNumber = "254" + phoneNumber.Remove(0, 2);
+                }
+                if (phoneNumber.StartsWith("0"))
+                {
+                    phoneNumber = "254" + phoneNumber.Remove(0, 1);
+                }
+                if (phoneNumber.StartsWith("25400") && phoneNumber.Length > 12)
+                {
+                    phoneNumber = "254" + phoneNumber.Remove(0, 5);
+                }
+                if (phoneNumber.StartsWith("2540254") && phoneNumber.Length > 12)
+                {
+                    phoneNumber = "254" + phoneNumber.Remove(0, 7);
+                }
+                if (phoneNumber.StartsWith("25400") && phoneNumber.Length > 12)
+                {
+                    phoneNumber = "254" + phoneNumber.Remove(0, 5);
+                }
+                if (phoneNumber.StartsWith("254254") && phoneNumber.Length > 12)
+                {
+                    phoneNumber = phoneNumber.Remove(0, 3);
+                }
+            }
+
+            return phoneNumber;
+        }
+        public static bool ValidateTransferType(string transferType)
+        {
+            if (string.IsNullOrEmpty(transferType))
+                return false;
+
+            if (transferType.ToLower().Contains("interbank") || transferType.ToLower().Contains("intrabank") || transferType.ToLower().Contains("ownaccount")
+                || transferType.ToLower().Contains("swift"))
+                return true;
+
+            return false;
+        }
+    }
+}
