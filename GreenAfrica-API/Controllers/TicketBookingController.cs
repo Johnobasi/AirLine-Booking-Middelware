538bad23cf72a53049a33e00c69ef066684732fe
Kabiru Okeleye <kabiotobiano@gmail.com>
2024-01-19 18:13:38 +0100

    Add AddTicketBooking

--- GreenAfrica-API/Controllers/TicketBookingController.cs
+++ GreenAfrica-API/Controllers/TicketBookingController.cs
@@ -1,4 +1,6 @@
-﻿using GreenAfricaAPI.Business.Abstract;
+﻿using GreenAfrica.DataAccess.Models;
+using GreenAfrica_API.Dtos;
+using GreenAfricaAPI.Business.Abstract;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GreenAfrica_API.Controllers
@@ -14,9 +16,36 @@ namespace GreenAfrica_API.Controllers
             _iTicketBookingService = iTicketBookingService;
         }
 
+        [HttpGet]
         public IActionResult GetAllTicketBookings()
         {
             var result = _iTicketBookingService.GetTicketBookingsAsync(e => true);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult AddTicketBookings(TicketBookingDto request)
+        {
+            var result = _iTicketBookingService.AddTicketBookingAsync
+                (
+                     new TicketBooking
+                     {
+                         ArrivalDate = request.ArrivalDate,
+                         Departure = request.Departure,
+                         DepartureDate = request.DepartureDate,
+                         Destination = request.Destination,
+                         FirstName = request.FirstName,
+                         EmailAddress = request.EmailAddress,
+                         Dob = request.Dob,
+                         EmergencyPhoneNumber = request.EmergencyPhoneNumber,
+                         Gender = request.Gender,
+                         LastName = request.LastName,
+                         Passport = request.Passport,
+                         PhoneNumber = request.PhoneNumber,
+                         TicketStatus = request.TicketStatus,
+                         IsInternational = request.IsInternational
+                     }
+                );
             return Ok(result);
         }
     }
