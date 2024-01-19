55ed475bd9038b07edffd9cc14c095aab23fa3de
Kabiru Okeleye <kabiotobiano@gmail.com>
2024-01-19 17:57:26 +0100

    Add Get all ticket booking

---
+++ GreenAfrica-API/Controllers/TicketBookingController.cs
@@ -0,0 +1,23 @@
+﻿using GreenAfricaAPI.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenAfrica_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketBookingController : ControllerBase
+    {
+        private readonly ITicketBookingService _iTicketBookingService;
+
+        public TicketBookingController(ITicketBookingService iTicketBookingService)
+        {
+            _iTicketBookingService = iTicketBookingService;
+        }
+
+        public IActionResult GetAllTicketBookings()
+        {
+            var result = _iTicketBookingService.GetTicketBookingsAsync(e => true);
+            return Ok(result);
+        }
+    }
+}
