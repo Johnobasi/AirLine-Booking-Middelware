a4b5b6416d9921ad6367c7ea11302aed6c1c45a9
Kabiru Okeleye <kabiotobiano@gmail.com>
2024-01-19 16:18:38 +0100

    add TicketBooking model

---
+++ GreenAfrica-API/Data/TicketBooking.cs
@@ -0,0 +1,10 @@
+﻿namespace GreenAfrica_API.Data
+{
+    public class TicketBooking
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public TicketStatus TicketStatus { get; set; }
+    }
+}
---
+++ GreenAfrica-API/Data/TicketStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace GreenAfrica_API.Data
+{
+    public enum TicketStatus
+    {
+        Available,
+        Confirmed,
+        Reserved,
+        Cancelled,
+        Complete
+    }
+}
