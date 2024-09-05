5f7c90ebf49fb424c78ce4ef9f777f68035a4c0b
Amenawon Esezobor <menaesezobor@gmail.com>
2024-09-05 23:20:38 +0100

    Create PassengerDTO.cs
    Create Passenger DTO clss

---
+++ GreenAfrica-API/Dtos/PassengerDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GreenAfrica_API.Dtos
+{
+    public class PassengerDTO
+    {
+        //should have a guid;
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
